1ae4a7b35feaef88c328f651fd5c74b488e677f6
johanngeustyn <info@johanngeustyn.co.za>
2023-06-04 13:48:47 +0200

    Authentication Commit

--- app/src/main/java/com/opsc/timesync/MainActivity.kt
+++ app/src/main/java/com/opsc/timesync/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.opsc.timesync
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.firebase.auth.FirebaseAuth
 import com.opsc.timesync.databinding.ActivityMainBinding
+import com.opsc.timesync.ui.LoginActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,10 +30,29 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_logout
             )
         )
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        navView.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.navigation_logout -> {
+                    FirebaseAuth.getInstance().signOut()
+
+                    // Redirect to LoginActivity after sign-out
+                    val intent = Intent(this, LoginActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(intent)
+                    true
+                }
+                else -> {
+                    NavigationUI.onNavDestinationSelected(menuItem, navController)
+                    true
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/opsc/timesync/ui/LoginActivity.kt
@@ -0,0 +1,106 @@
+package com.opsc.timesync.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.opsc.timesync.MainActivity
+import com.opsc.timesync.R
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        auth = Firebase.auth
+
+        val email = findViewById<EditText>(R.id.email)
+        val password = findViewById<EditText>(R.id.password)
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        val signUpText = findViewById<TextView>(R.id.signUpText)
+
+        loginButton.setOnClickListener {
+            signIn(email.text.toString(), password.text.toString())
+        }
+
+        signUpText.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun signIn(email: String, password: String) {
+        Log.d(TAG, "signIn:$email")
+        if (!validateForm()) {
+            return
+        }
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+    }
+
+    private fun validateForm(): Boolean {
+        var valid = true
+
+        val email = findViewById<EditText>(R.id.email)
+        if (TextUtils.isEmpty(email.toString())) {
+            email.error = "Required."
+            valid = false
+        } else {
+            email.error = null
+        }
+
+        val password = findViewById<EditText>(R.id.password)
+        if (TextUtils.isEmpty(password.toString())) {
+            password.error = "Required."
+            valid = false
+        } else {
+            password.error = null
+        }
+
+        return valid
+    }
+
+    companion object {
+        const val TAG = "EmailPassword"
+    }
+}
