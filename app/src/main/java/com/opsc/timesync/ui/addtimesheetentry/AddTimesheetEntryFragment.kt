ff1a29417ebe6a0d4fd462e87d2303fb80861c20
Ediz <53665610+edizyurdakul@users.noreply.github.com>
2023-06-05 00:00:40 +0200

    added datepicker and two time pickers to add entry view

--- app/src/main/java/com/opsc/timesync/ui/addtimesheetentry/AddTimesheetEntryFragment.kt
+++ app/src/main/java/com/opsc/timesync/ui/addtimesheetentry/AddTimesheetEntryFragment.kt
@@ -1,50 +1,44 @@
 package com.opsc.timesync.ui.addtimesheetentry
 
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.DatePicker
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.TimePicker
-import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModelProvider
-import com.opsc.timesync.R
 import com.opsc.timesync.databinding.FragmentAddtimesheetentryBinding
-import java.sql.Date
-import java.util.Calendar
+import java.util.*
 
-
-class AddTimesheetEntryFragment : Fragment(), DatePickerDialog.OnDateSetListener {
+class AddTimesheetEntryFragment : Fragment(), DatePickerDialog.OnDateSetListener,
+    TimePickerDialog.OnTimeSetListener {
 
     private var _binding: FragmentAddtimesheetentryBinding? = null
     private val binding get() = _binding!!
-    private lateinit var titleEditText: EditText
-    private lateinit var startDateTextView: TextView
-    private lateinit var endDateTimeEditText: EditText
-    private lateinit var descriptionEditText: EditText
+
     private lateinit var showDatePickerButton: Button
+    private lateinit var showStartTimePickerButton: Button
+    private lateinit var showEndTimePickerButton: Button
+
+    private lateinit var activeTimePickerButton: Button
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this)[AddTimesheetEntryViewModel::class.java]
+        val addTimesheetEntryViewModel =
+            ViewModelProvider(this).get(AddTimesheetEntryViewModel::class.java)
 
         _binding = FragmentAddtimesheetentryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         showDatePickerButton = binding.buttonShowDatePicker
-
         showDatePickerButton.setOnClickListener {
-            // Create a new DatePickerDialog instance
             val datePickerDialog = DatePickerDialog(
                 requireContext(),
                 this,
@@ -53,10 +47,35 @@ class AddTimesheetEntryFragment : Fragment(), DatePickerDialog.OnDateSetListener
                 Calendar.DAY_OF_MONTH
             )
             datePickerDialog.datePicker.minDate = System.currentTimeMillis()
-
-            // Show the dialog
             datePickerDialog.show()
         }
+
+        showStartTimePickerButton = binding.buttonShowStartTimePicker
+        showStartTimePickerButton.setOnClickListener {
+            activeTimePickerButton = showStartTimePickerButton
+            val startTimePicker = TimePickerDialog(
+                requireContext(),
+                this,
+                Calendar.getInstance().get(Calendar.HOUR_OF_DAY),
+                Calendar.getInstance().get(Calendar.MINUTE),
+                false
+            )
+            startTimePicker.show()
+        }
+
+        showEndTimePickerButton = binding.buttonShowEndTimePicker
+        showEndTimePickerButton.setOnClickListener {
+            activeTimePickerButton = showEndTimePickerButton
+            val endTimePicker = TimePickerDialog(
+                requireContext(),
+                this,
+                Calendar.getInstance().get(Calendar.HOUR_OF_DAY),
+                Calendar.getInstance().get(Calendar.MINUTE),
+                false
+            )
+            endTimePicker.show()
+        }
+
         return root
     }
 
@@ -65,14 +84,21 @@ class AddTimesheetEntryFragment : Fragment(), DatePickerDialog.OnDateSetListener
         _binding = null
     }
 
-    override fun onDateSet(view: DatePicker, year: Int, month: Int, dayOfMonth: Int) {
-        // Update the startDateTimeEditText with the selected date
-        binding.startDateTextView.text = String.format(
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        binding.buttonShowDatePicker.text = String.format(
             "%d-%02d-%02d",
             year,
             month + 1,
             dayOfMonth
         )
     }
-}
 
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        val selectedButton = activeTimePickerButton
+        if (selectedButton == showStartTimePickerButton) {
+            selectedButton.text = String.format("%d:%02d", hourOfDay, minute)
+        } else if (selectedButton == showEndTimePickerButton) {
+            selectedButton.text = String.format("%d:%02d", hourOfDay, minute)
+        }
+    }
+}
